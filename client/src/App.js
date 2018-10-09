e7090382586815b60573a645b702293d1e2cc4a7
mapledrive <fastmn@yahoo.com>
2018-10-09 20:05:24 +0500

    added chart.js react-chartjs-2 cleaned src

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,9 @@
-// @flow
-
-import React, { Component } from "react";
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "font-awesome/css/font-awesome.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+function App() {
+  return <h1>Hello</h1>;
 }
-
 export default App;
