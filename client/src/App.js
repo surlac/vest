7a25d7035bcaa0f6c4e93b5f569a6f26d91e737b
mapledrive <fastmn@yahoo.com>
2018-10-11 14:20:13 +0500

    static version of webapp

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,154 @@
 import React from "react";
-import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
+import "./App.css";
 
-function App() {
-  return <h1>Hello</h1>;
-}
+const App = props => {
+  return (
+    <div>
+      <nav class="navbar navbar-expand-xl bg-light navbar-light">
+        <a class="navbar-brand" href="index.htm" />
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#collapsibleNavbar"
+        >
+          <span class="navbar-toggler-icon" />
+        </button>
+        <div class="collapse navbar-collapse" id="collapsibleNavbar">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Currencies
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Crypto
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Commodities
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                American
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                European
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                International
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Stocks
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                US Sectors
+              </a>
+            </li>
+          </ul>
+          <ul class="navbar-nav ml-auto d-none d-xl-block">
+            <button type="button" class="btn btn-outline-info mr-2">
+              <i class="fa fa-heart-o mr-1" /> Signup
+            </button>
+            <button type="button" class="btn btn-outline-info">
+              <i class="fa fa-sign-in mr-1" /> Login
+            </button>
+          </ul>
+        </div>
+      </nav>
+
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-12 col-lg-9">
+            <div class="row">
+              <div class="col-12 col-md-7 mini-padding">
+                <div class="card">
+                  <div class="card-body">silver</div>
+                </div>
+              </div>
+              <div class="col-12 col-md-5 mini-padding">
+                <div class="card">
+                  <div class="card-body">search</div>
+                </div>
+              </div>
+              <div class="col-12 mini-padding">
+                <div class="card">
+                  <div class="card-body">buttons</div>
+                </div>
+              </div>
+              <div class="col-12 mini-padding">
+                <div class="card">
+                  <div class="card-body">canvas</div>
+                </div>
+              </div>
+              <div class="col-12 col-lg-6 mini-padding">
+                <div class="card">
+                  <div class="card-body">cumulative</div>
+                </div>
+              </div>
+              <div class="col-12 col-lg-6 mini-padding">
+                <div class="card">
+                  <div class="card-body">patterns</div>
+                </div>
+              </div>
+              <div class="col-12 mini-padding">
+                <div class="card">
+                  <div class="card-body">list drop</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-12 col-lg-3">
+            <div class="row">
+              <div class="col-12 mini-padding">
+                <div class="card">
+                  <div class="card-body">date picker</div>
+                </div>
+              </div>
+              <div class="col-12 mini-padding">
+                <div class="card">
+                  <div class="card-body">diagram</div>
+                </div>
+              </div>
+              <div class="col-12 mini-padding">
+                <div class="card">
+                  <div class="card-body">return</div>
+                </div>
+              </div>
+              <div class="col-12 mini-padding">
+                <div class="card">
+                  <div class="card-body">profit</div>
+                </div>
+              </div>
+              <div class="col-12 mini-padding">
+                <div class="card">
+                  <div class="card-body">gains losses</div>
+                </div>
+              </div>
+              <div class="col-12 mini-padding">
+                <div class="card">
+                  <div class="card-body">miscellaneous</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default App;
